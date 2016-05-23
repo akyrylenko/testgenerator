6340f18bc5fcf4b4b406c15956ec652123ff5876
Andriy Kyrylenko <andriy.kyrylenko@gmail.com>
2016-05-23 13:11:24 +0300

    SH-101 add API-Version to request header

--- lib/judo_payments_responder.rb
+++ lib/judo_payments_responder.rb
@@ -8,10 +8,11 @@ class JudoPaymentsResponder
     @auth = {:username => u, :password => p}
   end
 
-  # which can be :friends, :user or :public
-  # options[:query] can be things like since, since_id, count, etc.
   def webpayments(options={})
-    options.merge!({:basic_auth => @auth})
+    options.merge!({
+      headers: {'Api-Version' => '4.1.0'},
+      basic_auth: @auth
+    })
     self.class.post("/webpayments/payments", options)
   end
 end
